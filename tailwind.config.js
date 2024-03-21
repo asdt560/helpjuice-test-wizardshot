005916a39955eeedb7350c6856cc16886f2acdd0
JustoRago <103001263+asdt560@users.noreply.github.com>
2024-03-21 19:43:51 -0300

    Set up fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      open: ['"Open Sans"', 'sans-serif'],
+      jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+      mulish: ['Mulish', 'sans-serif'],
+      cassette: ['"Alphabetized Cassette Tapes Thin"', 'sans-serif'],
+    },
   },
   plugins: [],
 }
